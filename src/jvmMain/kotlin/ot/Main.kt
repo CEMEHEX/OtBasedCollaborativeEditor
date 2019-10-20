d522d35dc2fa74debc42b0e4e2ceca0152a0c7d7
Semyon <CEMEHEX@yandex.ru>
2019-10-21 00:40:07 +0300

    Configured spring

---
+++ src/jvmMain/kotlin/ot/Main.kt
@@ -0,0 +1,10 @@
+package ot
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OtApplication
+fun main(args: Array<String>) {
+    runApplication<OtApplication>(*args)
+}
