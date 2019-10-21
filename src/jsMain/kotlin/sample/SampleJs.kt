b43a0b3466bbcdad4c0476182d87ea57270d9c1f
Semyon Yekhmenin <s.yekhmenin@id-east.ru>
2019-10-21 13:19:02 +0300

    Removed samples

--- src/jsMain/kotlin/sample/SampleJs.kt
+++ src/jsMain/kotlin/sample/SampleJs.kt
@@ -2,19 +2,11 @@ package sample
 
 import kotlin.browser.*
 
-actual class Sample {
-    actual fun checkMe() = 12
-}
-
-actual object Platform {
-    actual val name: String = "JS"
-}
-
 
 @Suppress("unused")
 @JsName("helloWorld")
 fun helloWorld(salutation: String) {
-    val message = "$salutation from Kotlin.JS ${hello()}, check me value: ${Sample().checkMe()}"
+    val message = "$salutation from Kotlin.JS"
     document.getElementById("js-response")?.textContent = message
 }
 
