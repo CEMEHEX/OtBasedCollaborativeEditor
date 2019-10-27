d0d67eca9d5f480e3ac18c9cb257c62e7febd1e0
Semyon <CEMEHEX@yandex.ru>
2019-10-27 21:53:24 +0300

    Fixed problem with concurrent access to ServerDocumentManager

---
+++ src/jvmMain/kotlin/ot/config/ExecutorServiceConfig.kt
@@ -0,0 +1,12 @@
+package ot.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+
+@Configuration
+class ExecutorServiceConfig {
+    @Bean("SingleThreadExecutor")
+    fun singleThreadExecutorService() : ExecutorService = Executors.newSingleThreadExecutor()
+}
--- src/jvmMain/kotlin/ot/ws/OtWsController.kt
+++ src/jvmMain/kotlin/ot/ws/OtWsController.kt
@@ -2,6 +2,7 @@ package ot.ws
 
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.messaging.handler.annotation.DestinationVariable
 import org.springframework.messaging.handler.annotation.MessageMapping
 import org.springframework.messaging.handler.annotation.Payload
@@ -10,12 +11,14 @@ import org.springframework.stereotype.Controller
 import ot.entity.PlainTextDocument
 import ot.service.ServerDocumentManager
 import ot.service.impl.PlainTextSingleCharacterOperation
+import java.util.concurrent.ExecutorService
 
 val logger: Logger = LoggerFactory.getLogger(OtWsController::class.java)
 
 @Controller
 class OtWsController(
-    private val serverDocumentManager: ServerDocumentManager<String, PlainTextSingleCharacterOperation, PlainTextDocument>
+    private val serverDocumentManager: ServerDocumentManager<String, PlainTextSingleCharacterOperation, PlainTextDocument>,
+    @Qualifier("SingleThreadExecutor") private val singleThreadExecutor: ExecutorService
 ) {
 
     @MessageMapping("/{documentId}/operation")
@@ -23,21 +26,21 @@ class OtWsController(
     fun sendMessage(
         @Payload operation: PlainTextSingleCharacterOperation,
         @DestinationVariable documentId: Long
-    ): PlainTextSingleCharacterOperation {
+    ): PlainTextSingleCharacterOperation = singleThreadExecutor.submit<PlainTextSingleCharacterOperation> {
         logger.debug(
             """Operation received: 
                 $operation
                 Document: ${serverDocumentManager.getDocument(documentId)}
             """
         )
-        return serverDocumentManager.receiveOperation(documentId, operation).also {
+        return@submit serverDocumentManager.receiveOperation(documentId, operation).also {
             logger.debug(
                 """Transformed operation: 
-                    $it
-                    Document after operation applied: ${serverDocumentManager.getDocument(documentId)}
-                """
+                        $it
+                        Document after operation applied: ${serverDocumentManager.getDocument(documentId)}
+                    """
             )
         }
-    }
+    }.get() // TODO use reactive websockets
 
 }
